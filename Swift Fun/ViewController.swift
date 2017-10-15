b835e6cd2c31f296257567b5dcf06503be5088e1
Navish Chaudhari <navish916@yahoo.com>
2017-10-15 00:17:50 -0700

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Navishkaar Prasad Chaudhari on 10/15/17.
+//  Copyright © 2017 Navishkaar Prasad Chaudhari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
